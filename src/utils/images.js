918afc055b88d6ec317f9627cf59ea94c33501ca
Patrick Amuah <patrickamuah38@gmail.com>
2025-06-17 09:49:44 +0000

    gallery

---
+++ src/utils/images.js
@@ -0,0 +1,32 @@
+const images = [
+  {
+    id: 1,
+    src: "https://photutorial.com/wp-content/uploads/2023/04/Featured-image-AI-image-generators-by-Midjourney.png",
+    webp: "../assets/web3.webp",
+    alt: "A beautiful woman",
+    description: "Portrait of a woman with dramatic lighting.",
+  },
+  {
+    id: 2,
+    src: "https://images.pexels.com/photos/161293/prambanan-temple-java-hinduism-161293.jpeg",
+    webp: "../assets/web2.webp",
+    alt: "A temple building",
+    description: "A traditional Asian temple during daytime.",
+  },
+  {
+    id: 3,
+    src: "https://images.pexels.com/photos/709552/pexels-photo-709552.jpeg",
+    webp: "../assets/web1.webp",
+    alt: "River and village",
+    description: "Peaceful riverside village landscape.",
+  },
+  {
+    id: 4,
+    src: "https://images.pexels.com/photos/431722/pexels-photo-431722.jpeg",
+    webp: "../assets/web1.webp",
+    alt: "Street at night",
+    description: "Lantern-lit street at night.",
+  },
+];
+
+export default images;
